e19e838164d843560dfe5e9581bdd7af17ee4aa5
Serg890 <retub2014@gmail.com>
2020-08-10 11:25:18 +0300

    Updates

---
+++ precache-manifest.b8484e0dfbc80c16af48842e1f3b51e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7645921a5d19ad64d09034fd898915db",
+    "url": "/test-react-app/index.html"
+  },
+  {
+    "revision": "20e35eb377f2488e2d80",
+    "url": "/test-react-app/static/css/main.e43a1c31.chunk.css"
+  },
+  {
+    "revision": "62a3b49de55d9f3c826a",
+    "url": "/test-react-app/static/js/2.9c1b31fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-react-app/static/js/2.9c1b31fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e35eb377f2488e2d80",
+    "url": "/test-react-app/static/js/main.9322ef98.chunk.js"
+  },
+  {
+    "revision": "7ef761515c1d80a40831",
+    "url": "/test-react-app/static/js/runtime-main.c2148eca.js"
+  }
+]);
